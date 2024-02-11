04a7fc22476ec9f162f389375c784de3053b57b4
tomekm <tomasz.mazur@dimedic.eu>
2024-02-11 17:19:28 +0100

    fix tags for tests checks

--- src/app/components/organisms/ProductList.tsx
+++ src/app/components/organisms/ProductList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { type ProductType } from "../types";
 import { ProductItemImage } from "../atoms/ProductItemImage";
+import { ProductItemDescription } from "../atoms/ProductItemDescription";
 
 type ProductListType = {
 	products: ProductType[];
@@ -10,7 +11,12 @@ export const ProductList = ({ products }: ProductListType) => {
 	return (
 		<ul className="flex items-center justify-center gap-3" data-testid="products-list">
 			{products.map((product: ProductType) => (
-				<ProductItemImage product={product} key={product.id} />
+				<li key={product.id}>
+					<article>
+						<ProductItemImage product={product} />
+						<ProductItemDescription product={product} />
+					</article>
+				</li>
 			))}
 		</ul>
 	);
--- src/app/components/types.ts
+++ src/app/components/types.ts
@@ -2,6 +2,7 @@ export type ProductType = {
 	id: string;
 	category: string;
 	price: number;
+	name: string;
 	coverImage: {
 		src: string;
 		alt: string;
