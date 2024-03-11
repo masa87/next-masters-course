eccc79008faa190c3ebd5736dc86a0a3d4d1c7e7
Tomasz Mazur <tomasz.mazur87@gmail.com>
2024-03-11 22:57:42 +0100

    color changed (#16)

--- src/app/components/atoms/CustomButton.tsx
+++ src/app/components/atoms/CustomButton.tsx
@@ -19,7 +19,7 @@ const CustomButton = ({
 			type="submit"
 			data-testid={dataTestId}
 			disabled={status.pending}
-			className="rounded-md bg-blue-500 px-4 py-2 font-semibold text-white transition hover:bg-blue-600 disabled:cursor-not-allowed disabled:bg-blue-400"
+			className="rounded-md bg-blue-600 px-4 py-2 font-semibold text-white transition hover:bg-blue-600 disabled:cursor-not-allowed disabled:bg-blue-400"
 		>
 			{title || children}
 		</button>
